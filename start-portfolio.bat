b3fe2a6f3071528ff112777702b48eea0e16b667
Ammar Ayyaz <ammarayyaz47@gmail.com>
2026-09-22 12:55:45 +0500

    Configure package.json, clean static assets, and rename dev server

--- start-portfolio.bat
+++ start-portfolio.bat
@@ -4,5 +4,5 @@ echo ======================================================
 echo   Starting Minhaj Rasool Portfolio Server...
 echo ======================================================
 start "" "http://localhost:3001/"
-node server.js
+node dev-server.js
 pause
